cce239c6a967fcf999073905fddcea6202933d6b
toan-dp <57466723+toan-dp@users.noreply.github.com>
2020-10-07 00:28:59 +0700

    Test_lab02

---
+++ My First Web UI Project/Test Suites/Lab.ts
@@ -0,0 +1,68 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Lab</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient>;dangphuctoan.k43cnttc.hcmue@gmail.com;testuiweb@gmail.com;</mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>0daf9876-1cbd-41db-82ae-60b6cd38af71</testSuiteGuid>
+   <testCaseLink>
+      <guid>bcea3151-712d-41c7-bc18-1c77a87a9130</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test Case 1</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>570007de-5372-43c1-9be4-7d392c3e1b84</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>0a81eb79-3cf3-4044-a06d-e6cce93de5c1</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ee255876-e005-4aa7-93d5-d69e5ac15300</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test Case 2</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>3d0141f9-579c-47d6-9852-7fd582d29ee8</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>2d0e4f27-da27-4b9c-9724-ed929dec8f2a</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>8b046986-9687-4596-8b70-792f55d9ab50</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test Case 4</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e54c0ede-e64a-4924-bfb3-9fdfae9f902c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test Case 3</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>29e70a42-e5da-489e-b197-d04f9ffad043</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test Case 5</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
